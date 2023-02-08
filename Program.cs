90531c8451b4d24cfed9ff75ba16b5900881d33a
Petro_witch <wonkewood@gmail.com>
2023-02-08 19:29:02 +0300

    Up

--- Program.cs
+++ Program.cs
@@ -29,10 +29,10 @@ System.Console.Write(SumOfAll(1, 15));
 
 //Нашёл формулы, но так и не понял как вообще это работает и что делается :D
 int Akkerman(int n, int m)
-      {
-          if (n == 0) return m + 1;
-          if (n != 0 && m == 0) return Akkerman(n - 1, 1);
-          if (n > 0 && m > 0) return Akkerman(n - 1, Akkerman(n, m - 1));
-          return Akkerman(n,m);
-      }
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return Akkerman(n - 1, 1);
+    if (n > 0 && m > 0) return Akkerman(n - 1, Akkerman(n, m - 1));
+    return Akkerman(n,m);
+}
     Console.WriteLine(Akkerman(2, 2));
